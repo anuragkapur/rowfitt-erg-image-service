5ab734cffa90da069933df45c7aa880c41536d98
Anurag Kapur <anurag@beancrunch.com>
2018-10-11 00:33:05 +0100

    Work in progress text to Workout translation

--- src/test/java/com/beancrunch/rowfitt/gcp/GCPCloudVisionImageServiceTest.java
+++ src/test/java/com/beancrunch/rowfitt/gcp/GCPCloudVisionImageServiceTest.java
@@ -1,70 +1,54 @@
 package com.beancrunch.rowfitt.gcp;
 
-import com.beancrunch.rowfitt.ErgImageService;
 import com.beancrunch.rowfitt.domain.Workout;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Base64;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
+import java.util.stream.Stream;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static java.nio.file.Paths.get;
 
 @ExtendWith(MockitoExtension.class)
 class GCPCloudVisionImageServiceTest {
 
     @Mock
-    GCPCloudVisionClient gcpCloudVisionClient = new GCPCloudVisionClient();
+    GCPCloudVisionClient gcpCloudVisionClient;
 
-    private static String singleDistanceWorkoutImageText1;
-
-    @BeforeEach
-    void setUp() {
-        singleDistanceWorkoutImageText1 = "view Detail\n" +
-                "4000m\n" +
-                "Mar 13 2017\n" +
-                "time meter 1500m-m\n" +
-                "16:10.8 4000 2013 19\n" +
-                "3:143 800 21014 19\n" +
-                "314.4 1600 2101,5 19\n" +
-                "3:15 2400 2102.3 19\n" +
-                "3:15.3 3200 2:02.0 20\n" +
-                "3:11.1 4000 1:59,4 21\n" +
-                "CHANGE\n" +
-                "UNITS\n" +
-                "CHANGE\n" +
-                "DISPLAY\n" +
-                "MEN\n" +
-                "ВАС";
-    }
+    private GCPCloudVisionImageService gcpCloudVisionImageService = new GCPCloudVisionImageService();
 
     @Test
-    void getWorkoutFromSingleDistanceImage1() throws IOException {
-        when(gcpCloudVisionClient.getTextFromImage(any())).thenReturn(Optional.of(singleDistanceWorkoutImageText1));
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("erg-single-distance1.jpg");
-        System.out.println(IOUtils.toByteArray(inputStream).length);
-        ErgImageService ergImageService = new GCPCloudVisionImageService(gcpCloudVisionClient);
-        Workout workout = ergImageService.getWorkoutFromImage(new byte[0]);
-        Assertions.assertNotNull(workout, "workout extracted from image must not be null");
-        Assertions.assertEquals("", workout.toString(), "extracted workout attributes must be as expected");
+    void testWorkoutTextSummaryExtraction() throws Exception {
+        Optional<URL> ergTextsDirUrl = Optional.ofNullable(getClass().getClassLoader().getResource("temp-out"));
+        ergTextsDirUrl
+                .map(url -> getChildPathStream(get(url.getPath())))
+                .orElseThrow(() -> new Exception("No files found to process"))
+                .forEach(this::testWorkoutExtractionForFile);
     }
 
-    @Test
-    void bla() throws IOException {
-       
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("encodedImageString.txt");
-        byte[] imageStringBytes = IOUtils.toByteArray(inputStream);
-        byte[] imageBytes = Base64.getDecoder().decode(imageStringBytes);
-        FileUtils.writeByteArrayToFile(new File("/tmp/image.jpeg"), imageBytes);
+    private static Stream<Path> getChildPathStream(Path path) {
+        try {
+            return Files.list(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stream.empty();
+    }
+
+    private void testWorkoutExtractionForFile(Path path) {
+        try {
+            System.out.println(path.getFileName());
+            String lines = new String(Files.readAllBytes(path));
+            Workout workout = gcpCloudVisionImageService.getWorkout(lines);
+            System.out.println(workout.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
