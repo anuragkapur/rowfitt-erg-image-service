7020ca87fb39096aa1120bb71e6024b632a5d063
Anurag Kapur <anurag@beancrunch.com>
2018-10-11 23:29:24 +0100

    Change default values, type of split, add toJsonString

--- src/main/java/com/beancrunch/rowfitt/domain/Workout.java
+++ src/main/java/com/beancrunch/rowfitt/domain/Workout.java
@@ -1,6 +1,8 @@
 package com.beancrunch.rowfitt.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,19 +12,24 @@ import java.util.UUID;
 public class Workout {
 
     private String workoutId;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date date;
-    private int distance;
-    private int timeHh;
-    private int timeMm;
-    private float timeSss;
-    private float splitMm;
-    private float splitSss;
-    private int strokeRate;
-    private int heartRate;
+    private int distance = -1;
+    private int timeHh = -1;
+    private int timeMm = -1;
+    private float timeSss = -1;
+    private int splitMm = -1;
+    private float splitSss = -1;
+    private int strokeRate = -1;
+    private int heartRate = -1;
     private String userId;
+    private String workoutType;
 
     public Workout() {
         workoutId = UUID.randomUUID().toString();
+    }
+
+    public String toJsonString() throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(this);
     }
 }
