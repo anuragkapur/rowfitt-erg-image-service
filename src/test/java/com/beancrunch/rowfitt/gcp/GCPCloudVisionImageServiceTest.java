76b5f826e23971dfb061bc1557d097409c02b5e7
Anurag Kapur <anurag@beancrunch.com>
2018-10-11 23:31:22 +0100

    Write unit test to assert extracted value of each field across all test files

--- src/test/java/com/beancrunch/rowfitt/gcp/GCPCloudVisionImageServiceTest.java
+++ src/test/java/com/beancrunch/rowfitt/gcp/GCPCloudVisionImageServiceTest.java
@@ -1,35 +1,66 @@
 package com.beancrunch.rowfitt.gcp;
 
 import com.beancrunch.rowfitt.domain.Workout;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import static java.lang.String.valueOf;
 import static java.nio.file.Paths.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class GCPCloudVisionImageServiceTest {
 
-    @Mock
-    GCPCloudVisionClient gcpCloudVisionClient;
+    private static final String expectedWorkoutsDir =
+            "/Users/anuragkapur/tech-stuff/workspace/beancrunch/rowfitt-erg-image-service/src/test/resources/erg" +
+                    "-expected-workouts/";
 
-    private GCPCloudVisionImageService gcpCloudVisionImageService = new GCPCloudVisionImageService();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-    @Test
-    void testWorkoutTextSummaryExtraction() throws Exception {
+    private static GCPCloudVisionImageService gcpCloudVisionImageService = new GCPCloudVisionImageService();
+
+    private static List<ExpectedActualPair> pairs = new ArrayList<>();
+
+    @BeforeAll
+    static void setup() {
+        try {
+            new GCPCloudVisionImageServiceTest().extractWorkoutsFromText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPairsStream")
+    void testPairs(ExpectedActualPair pair) {
+        assertEquals(pair.expected, pair.actual,
+                String.format("Actual not as expected for '%s' in %s", pair.field, pair.fileName));
+    }
+
+    private static Stream<ExpectedActualPair> getPairsStream() {
+        return pairs.stream();
+    }
+
+    private void extractWorkoutsFromText() throws Exception {
         Optional<URL> ergTextsDirUrl = Optional.ofNullable(getClass().getClassLoader().getResource("temp-out"));
         ergTextsDirUrl
                 .map(url -> getChildPathStream(get(url.getPath())))
                 .orElseThrow(() -> new Exception("No files found to process"))
-                .forEach(this::testWorkoutExtractionForFile);
+                .forEach(this::initExpectedActualPairsForFile);
     }
 
     private static Stream<Path> getChildPathStream(Path path) {
@@ -41,14 +72,58 @@ class GCPCloudVisionImageServiceTest {
         return Stream.empty();
     }
 
-    private void testWorkoutExtractionForFile(Path path) {
+    private void initExpectedActualPairsForFile(Path path) {
         try {
-            System.out.println(path.getFileName());
+            String fileName = path.getFileName().toString();
             String lines = new String(Files.readAllBytes(path));
-            Workout workout = gcpCloudVisionImageService.getWorkout(lines);
-            System.out.println(workout.toString());
+            Workout actual = gcpCloudVisionImageService.getWorkout(lines);
+            Workout expected = getExpectedWorkout(path);
+            addPairs(fileName, actual, expected);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void addPairs(String fileName, Workout actual, Workout expected) {
+        pairs.add(new ExpectedActualPair(valueOf(expected.getDate()), valueOf(actual.getDate()), fileName, "date"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getDistance()), valueOf(actual.getDistance()), fileName,
+                "distance"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getSplitMm()), valueOf(actual.getSplitMm()), fileName,
+                "splitMm"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getSplitSss()), valueOf(actual.getSplitSss()), fileName,
+                "SplitSss"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getTimeHh()), valueOf(actual.getTimeHh()), fileName,
+                "TimeHh"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getTimeMm()), valueOf(actual.getTimeMm()), fileName,
+                "TimeMm"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getTimeSss()), valueOf(actual.getTimeSss()), fileName,
+                "TimeSss"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getStrokeRate()), valueOf(actual.getStrokeRate()),
+                fileName, "strokeRate"));
+        pairs.add(new ExpectedActualPair(valueOf(expected.getHeartRate()), valueOf(actual.getHeartRate()), fileName,
+                "heartRate"));
+        pairs.add(new ExpectedActualPair(expected.getWorkoutType(), actual.getWorkoutType(), fileName, "workoutType"));
+    }
+
+    private Workout getExpectedWorkout(Path path) throws IOException {
+        return objectMapper.readValue(new File(expectedWorkoutsDir+getWorkoutFileName(path)), Workout.class);
+    }
+
+    private String getWorkoutFileName(Path path) {
+        return path.getFileName().toString().replace(".txt", ".json");
+    }
+
+    private class ExpectedActualPair {
+        private String expected;
+        private String actual;
+        private String fileName;
+        private String field;
+
+        ExpectedActualPair(String expected, String actual, String fileName, String field) {
+            this.expected = expected;
+            this.actual = actual;
+            this.fileName = fileName;
+            this.field = field;
         }
     }
 }
